70d95c1892c4e3b74875c571722872b25b9dd928
MForever78 <shunjian1128@gmail.com>
2016-09-24 16:34:57 +0800

    feat: generate inserts, deletes, quries dependently

--- generator.go
+++ generator.go
@@ -33,21 +33,26 @@ func buildDictionary() {
 func main() {
 	buildDictionary()
 
-	var n int
-	fmt.Scanf("%d", &n)
+	var inserts, deletes, queries int
+	fmt.Scanf("%d %d %d", &inserts, &deletes, &queries)
 
 	r := rand.New(rand.NewSource(233))
-	for i := 0; i < n; i++ {
-		op := r.Intn(3)
+	for i := 0; i < inserts; i++ {
+		op := 0
+		index := i
+		valueIndex := r.Intn(len(dictionary))
+		fmt.Printf("%d %s %s\n", op, dictionary[index], dictionary[valueIndex])
+	}
+
+	for i := 0; i < deletes; i++ {
+		op := 1
 		index := r.Intn(len(dictionary))
-		// 0: insert
-		// 1: remove
-		// 2: query
-		if op == 0 {
-			valueIndex := r.Intn(len(dictionary))
-			fmt.Printf("%d %s %s\n", op, dictionary[index], dictionary[valueIndex])
-		} else {
-			fmt.Printf("%d %s\n", op, dictionary[index])
-		}
+		fmt.Printf("%d %s\n", op, dictionary[index])
+	}
+
+	for i := 0; i < queries; i++ {
+		op := 2
+		index := r.Intn(len(dictionary))
+		fmt.Printf("%d %s\n", op, dictionary[index])
 	}
 }
